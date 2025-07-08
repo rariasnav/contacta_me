a0808a138553bfa3c64fd28348053f4dfde3c674
Rafael Arias <ariasnavarrorafael@gmail.com>
2025-07-08 12:24:34 -0500

    commit

--- lib/utils/call_helper.dart
+++ lib/utils/call_helper.dart
@@ -1,9 +1,20 @@
+import 'dart:io' show Platform;
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CallHelper {
   static Future<void> makeEmergencyCall(String phoneNumber) async {
     try {
-      await FlutterPhoneDirectCaller.callNumber(phoneNumber);
+      if (Platform.isAndroid) {
+        await FlutterPhoneDirectCaller.callNumber(phoneNumber);
+      } else if (Platform.isIOS) {
+        final uri = Uri(scheme: 'tel', path: phoneNumber);
+        if (await canLaunchUrl(uri)) {
+          await launchUrl(uri);
+        } else {
+          throw("No se puede realizar la llamada");
+        }
+      }
     } catch (e) {
       throw("Error al realizar la llamada: $e");
     }
